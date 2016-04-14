339856e47b23fb6464091a31b5cda029103c5d7c
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-04-14 10:45:26 -0600

    Added environment variables.

--- src/_config.js
+++ src/_config.js
@@ -3,7 +3,7 @@ var config = {};
 config.mongoURI = {
   test: 'mongodb://localhost/first-mean-app-testing',
   development: 'mongodb://localhost/first-mean-app',
-  production: process.env.DATABASE_URL
+  production: process.env.MONGODB_URI
 };
 
 module.exports = config;
